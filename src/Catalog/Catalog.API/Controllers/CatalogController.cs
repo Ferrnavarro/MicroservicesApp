9a7234d5bf4f0beeefbbbf395938a1b99a39ccba
maganf01 <fernando.magana@tecoloco.com>
2021-02-05 10:16:14 -0600

    Added business layer of catalog service

---
+++ src/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Catalog.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CatalogController: ControllerBase
+    {
+        private readonly IProductRepository _repository;
+
+        public CatalogController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+
+        
+
+
+    }
+}
---
+++ src/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreconfiguredProducts());
+            }
+
+        }
+
+        private static IEnumerable<Product> GetPreconfiguredProducts()
+        {
+            return new List<Product>()
+            {
+                new Product 
+                { 
+                    Name = "Iphone 12", 
+                    Summary = "asd", 
+                    Description = "Asd",
+                    ImageFile = "product-1.png",
+                    Price = 950.00M,
+                    Category = "Smartphone"
+                },
+
+                new Product
+                {
+                    Name = "Galaxy S21",
+                    Summary = "asd",
+                    Description = "Asd",
+                    ImageFile = "product-2.png",
+                    Price = 950.00M,
+                    Category = "Smartphone"
+                },
+
+                new Product
+                {
+                    Name = "Oneplus 8 Pro",
+                    Summary = "asd",
+                    Description = "Asd",
+                    ImageFile = "product-3.png",
+                    Price = 950.00M,
+                    Category = "Smartphone"
+                },
+
+                new Product
+                {
+                    Name = "Pixel 5",
+                    Summary = "asd",
+                    Description = "Asd",
+                    ImageFile = "product-4.png",
+                    Price = 950.00M,
+                    Category = "Smartphone"
+                },
+
+
+
+            };
+        }
+    }
+}
